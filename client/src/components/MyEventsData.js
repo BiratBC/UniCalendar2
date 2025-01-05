39597bfedbd61e875555dbed1b78b365e73e23a1
BiratBC <104758456+BiratBC@users.noreply.github.com>
2025-01-05 23:57:09 +0545

    minor changes

--- client/src/components/MyEventsData.js
+++ client/src/components/MyEventsData.js
@@ -1,7 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function MyEventsData(props) {
-
   const [statusColor, setStatusColor] = useState("");
   //https://upload.wikimedia.org/wikipedia/commons/f/ff/Green_icon.svg
   //red : https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt3QOYNQV9XQdX8OigPFftFkxVoGUcVsa17Q&s
@@ -9,35 +8,51 @@ function MyEventsData(props) {
 
   const handleStatusColor = () => {
     if (props.eventStatus === "upcoming") {
-      setStatusColor("https://upload.wikimedia.org/wikipedia/commons/f/ff/Green_icon.svg");
-    }
-    else if (props.eventStatus === "ongoing") {
+      setStatusColor(
+        "https://upload.wikimedia.org/wikipedia/commons/f/ff/Green_icon.svg"
+      );
+    } else if (props.eventStatus === "ongoing") {
       setStatusColor("https://www.svgrepo.com/show/408266/yellow-circle.svg");
+    } else {
+      setStatusColor(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt3QOYNQV9XQdX8OigPFftFkxVoGUcVsa17Q&s"
+      );
     }
-    else{
-      setStatusColor("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt3QOYNQV9XQdX8OigPFftFkxVoGUcVsa17Q&s");
-    }
-  }
+  };
+
 
   useEffect(() => {
     handleStatusColor();
-  }, [])
-  
+  }, []);
 
   return (
     <>
-    <div className="eventsTable">
-            <a href="/profile/manage-my-events">
-              <div className="table-items">
-                <div className="table-item">{(props.eventTitle).charAt(0).toUpperCase() + (props.eventTitle).slice(1)}</div>
-                <div className="table-item">{props.eventDate}</div>
-                <div className="table-item">{props.eventTime}</div>
-                <div className="table-item"><img className='mx-2' src={statusColor} alt="" style={{width : 20, height : 20}}/>{(props.eventStatus).charAt(0).toUpperCase() + (props.eventStatus).slice(1)}</div>
-              </div>
-            </a>
+      <div className="eventsTable">
+        <a href="/profile/manage-my-events">
+          <div className="table-items">
+            <div className="table-item">
+              {props.eventTitle.charAt(0).toUpperCase() +
+                props.eventTitle.slice(1)}
+            </div>
+            <div className="table-item">{props.eventDate.slice(0, 10)}</div>
+            <div className="table-item">
+              {props.eventTime}
+            </div>
+            <div className="table-item">
+              <img
+                className="mx-2"
+                src={statusColor}
+                alt=""
+                style={{ width: 20, height: 20 }}
+              />
+              {props.eventStatus.charAt(0).toUpperCase() +
+                props.eventStatus.slice(1)}
+            </div>
           </div>
+        </a>
+      </div>
     </>
-  )
+  );
 }
 
-export default MyEventsData
+export default MyEventsData;
